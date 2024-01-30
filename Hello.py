312146ef9d2826a32ed982368720ee1d53af0cce
Mark Lee <leemark@gmail.com>
2024-01-30 03:31:21 +0000

    key

--- Hello.py
+++ Hello.py
@@ -3,7 +3,7 @@ from langchain_openai import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)

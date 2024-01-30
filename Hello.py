4bd67760e7fc25302fa246a369ec1bbe8dba5f6f
Mark Lee <leemark@gmail.com>
2024-01-30 03:56:15 +0000

    q and a

--- Hello.py
+++ Hello.py
@@ -1,18 +1,37 @@
 import streamlit as st
+import re
 from langchain_openai import OpenAI
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+from langchain_community.document_loaders import WebBaseLoader 
+from langchain.indexes import VectorstoreIndexCreator
 
-st.title('🦜🔗 Quickstart App')
+st.title('🐯 Tiger Talk')
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+  srch_query = f"{input_text} site:coloradocollege.edu"
+  wrapper = DuckDuckGoSearchAPIWrapper(max_results=12)
+  search = DuckDuckGoSearchResults(api_wrapper=wrapper, source="text")
+  context = search.run(srch_query)
+  links = re.findall(r'https?://\S+', context)
+  cleaned_links = [link.rstrip("],") for link in links]
+  loader_list = []
+  for i in cleaned_links:
+    loader_list.append(WebBaseLoader(i))
+  index = VectorstoreIndexCreator().from_loaders(loader_list)
+  prompt = '''
+    Given the context, please provide an answer to {input_text}
+  '''
+  #st.info(llm(input_text))
+  print(prompt)
+  ans = index.query(question=query, llm=llm)
+  st.info(ans)
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  text = st.text_area('Enter text:', 'What is the Block Plan?')
   submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
+  if submitted:
     generate_response(text)
